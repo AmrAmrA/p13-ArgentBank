2a09779d6c91d4e2f5beab7d29c27817f6c5e2a6
AmrAmrA <amrelfattah.dev@gmail.com>
2023-11-11 01:47:54 +0100

    start the edition name feature

--- Frontend/my-app/src/components/UserHeader.jsx
+++ Frontend/my-app/src/components/UserHeader.jsx
@@ -1,15 +1,58 @@
-import React from "react";
-import {useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 export default function UserHeader() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedFirstName, setEditedFirstName] = useState('');
+  const [editedLastName, setEditedLastName] = useState('');
   const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setEditedFirstName(user.body.firstName);
+    setEditedLastName(user.body.lastName);
+  };
+
+  const handleSave = () => {
+    if (editedFirstName !== user.body.firstName || editedLastName !== user.body.lastName) {
+    }
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
+
   return (
     <header className="header">
       <h1>
         Welcome back
         <br />
-        {user.body.firstName} {user.body.lastName}
+        {isEditing ? (
+          <>
+            <input
+              type="text"
+              value={editedFirstName}
+              onChange={(e) => setEditedFirstName(e.target.value)}
+              placeholder="First Name"
+            />
+            <input
+              type="text"
+              value={editedLastName}
+              onChange={(e) => setEditedLastName(e.target.value)}
+              placeholder="Last Name"
+            />
+            <button onClick={handleSave}>Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+          </>
+        ) : (
+          <>
+            {user.body.firstName} {user.body.lastName}
+          </>
+        )}
       </h1>
-      <button className="edit-button">Edit Name</button>
+        <button onClick={handleEditClick} className="edit-button">Edit Name</button>
     </header>
   );
 }
